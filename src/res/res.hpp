4f237fb832b4acf46e8c5033b4a51d585a21666f
AWE Henry <mouth11or12@outlook.com>
2021-09-14 16:22:59 +0800

    Init resource manager

---
+++ src/res/res.hpp
@@ -0,0 +1,35 @@
+// Author: HenryAWE
+// License: The 3-clause BSD License
+
+#ifndef TESTWORLD_RES_RES_HPP
+#define TESTWORLD_RES_RES_HPP
+
+#include <map>
+#include <optional>
+#include "../renderer/renderer.hpp"
+
+
+namespace awe
+{
+    class ResourceManager
+    {
+    public:
+        ResourceManager();
+        ~ResourceManager();
+
+        static ResourceManager& Instance();
+
+        Texture& AddTexture(
+            const std::string& name,
+            Texture&& tex
+        );
+        /* Will return nullptr when required texture cannot be found */
+        Texture* GetTexturePtr(const std::string& name);
+        std::map<std::string, Texture>& GetTextures();
+
+    private:
+        std::map<std::string, Texture> m_tex;
+    };
+}
+
+#endif
