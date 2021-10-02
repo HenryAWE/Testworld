4871a2a7f97f21da59e2841ef68c57f650d55fe6
AWE Henry <mouth11or12@outlook.com>
2021-10-02 08:08:57 +0800

    Bug fix

--- src/script/callconv.hpp
+++ src/script/callconv.hpp
@@ -4,12 +4,12 @@
 #ifndef TESTWORLD_SCRIPT_CALLCONV_HPP
 #define TESTWORLD_SCRIPT_CALLCONV_HPP
 
-#ifdef _MSV_VER
+#ifdef _MSC_VER
 #   define TW_CDECL __cdecl
 #elif defined(__GNUC__)
 #   define TW_CDECL __attribute__((__cdecl__))
 #else
-#   warning "MacroTW_CDECL is not explicitly defined, may causing unwanted behavior"
+// WARNING: Macro TW_CDECL is not explicitly defined, may causing unwanted behavior
 #   define TW_CDECL
 #endif
 
