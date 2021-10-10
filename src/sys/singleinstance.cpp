597c18069530af72e67be916aba1fe9a6f7316f2
AWE Henry <mouth11or12@outlook.com>
2021-10-10 17:01:15 +0800

    Single instance guard on Linux

--- src/sys/singleinstance.cpp
+++ src/sys/singleinstance.cpp
@@ -2,12 +2,17 @@
 // License: The 3-clause BSD License
 
 #include "singleinstance.hpp"
+#include <SDL.h>
 #ifdef _WIN32
 #   define WIN32_LEAN_AND_MEAN 1
 #   include <Windows.h>
 #   include <filesystem>
+#elif defined(__LINUX__)
+#   include <sys/file.h>
+#   include <unistd.h>
+#   include <errno.h>
 #endif
-#include <SDL.h>
+#include <fmt/core.h>
 
 
 namespace awe
@@ -61,6 +66,62 @@ namespace awe
                 CloseHandle(handle);
                 handle = NULL;
             }
+        }
+#       elif defined(__LINUX__)
+        static int pid_file = 0;
+        static int lock = 0;
+        bool Acquire()
+        {
+            #define TW_PIDFILE_PATH "/tmp/testworld.pid"
+            pid_file = open(TW_PIDFILE_PATH, O_CREAT | O_RDWR, 0666);
+            if(pid_file == -1)
+            {
+                SDL_ShowSimpleMessageBox(
+                    SDL_MESSAGEBOX_ERROR,
+                    "Error",
+                    fmt::format(
+                        "Failed to open " TW_PIDFILE_PATH " : {}",
+                        strerror(errno)
+                    ).c_str(),
+                    nullptr
+                );
+
+                return false;
+            }
+            lock = flock(pid_file, LOCK_EX | LOCK_NB);
+            if(lock)
+            {
+                if(errno == EWOULDBLOCK)
+                {
+                    SDL_ShowSimpleMessageBox(
+                        SDL_MESSAGEBOX_ERROR,
+                        "EWOULDBLOCK",
+                        TW_PIDFILE_PATH " is acquired by other process, "
+                        "you probably have already open a Testworld application",
+                        nullptr
+                    );
+
+                    return false;
+                }
+
+                SDL_ShowSimpleMessageBox(
+                    SDL_MESSAGEBOX_ERROR,
+                    "Error",
+                    "Could not acquire " TW_PIDFILE_PATH,
+                    nullptr
+                );
+
+                return false;
+            }
+
+            return true;
+        }
+        void Release()
+        {
+            flock(lock, LOCK_UN);
+            lock = 0;
+            close(pid_file);
+            pid_file = 0;
         }
 #       else
         bool Acquire()
