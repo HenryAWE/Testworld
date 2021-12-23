bd2ca40e39ba997982d1c6c6a6a438f897b4e378
AWE Henry <mouth11or12@outlook.com>
2021-12-24 01:33:17 +0800

    Common graphic codes

---
+++ src/graphic/common/image.cpp
@@ -0,0 +1,63 @@
+// Author: HenryAWE
+// License: The 3-clause BSD License
+
+#include "image.hpp"
+#include <stb_image.h>
+#include <stb_image_write.h>
+
+
+namespace awe::graphic::common
+{
+    namespace detailed
+    {
+        ImageBase::~ImageBase() noexcept
+        {
+            Release();
+        }
+
+        bool ImageBase::LoadFile(const char* file, int desired_channels, int* channel)
+        {
+            stbi_set_flip_vertically_on_load(true);
+            glm::ivec2 size(0);
+            void* tmp = stbi_load(file, &size[0], &size[1], channel, desired_channels);
+            if(!tmp)
+            {
+                return false;
+            }
+
+            Release();
+            m_raw_data = tmp;
+            m_size = size;
+
+            return true;
+        }
+        bool ImageBase::LoadMemory(
+            const void* mem,
+            std::size_t length,
+            int desired_channels,
+            int* channel
+        ) {
+            glm::ivec2 size(0);
+            stbi_uc* tmp = stbi_load_from_memory(
+                static_cast<const stbi_uc*>(mem),
+                static_cast<int>(length),
+                &size[0], &size[1], channel, desired_channels
+            );
+            if(!tmp)
+            {
+                return false;
+            }
+
+            Release();
+            m_raw_data = tmp;
+            m_size = size;
+
+            return true;
+        }
+        void ImageBase::Release() noexcept
+        {
+            stbi_image_free(m_raw_data);
+            m_raw_data = nullptr;
+        }
+    }
+}
---
+++ src/graphic/common/image.hpp
@@ -0,0 +1,81 @@
+// Author: HenryAWE
+// License: The 3-clause BSD License
+
+#ifndef TESTWORLD_GRAPHIC_COMMON_IMAGE_HPP
+#define TESTWORLD_GRAPHIC_COMMON_IMAGE_HPP
+
+#include <filesystem>
+#include <utility>
+#include <glm/vec2.hpp>
+
+
+namespace awe::graphic::common
+{
+    namespace detailed
+    {
+        class ImageBase
+        {
+        public:
+            ~ImageBase() noexcept;
+
+            [[nodiscard]]
+            glm::ivec2 Size() const noexcept
+            {
+                return m_size;
+            }
+
+        protected:
+            bool LoadFile(
+                const char* file,
+                int desired_channels,
+                int* channel = nullptr
+            );
+            bool LoadMemory(
+                const void* mem,
+                std::size_t length,
+                int desired_channels,
+                int* channel = nullptr
+            );
+            void Release() noexcept;
+
+            [[nodiscard]]
+            constexpr void* RawData() const noexcept { return m_raw_data; }
+
+        private:
+            glm::ivec2 m_size = glm::ivec2(0);
+            void* m_raw_data = nullptr;
+        };
+    }
+
+    template <typename Format = std::byte, uint8_t Channel = 4>
+    class Image : public detailed::ImageBase
+    {
+    public:
+        typedef Format DataType;
+
+        static constexpr uint8_t CHANNEL = Channel;
+
+        bool Load(const std::filesystem::path& file)
+        {
+            return LoadFile(file.u8string().c_str(), Channel);
+        }
+        bool Load(const std::vector<std::byte>& data)
+        {
+            return LoadMemory(data.data(), data.size(), Channel);
+        }
+
+        DataType& operator[](glm::uvec2 coord)
+        {
+            const std::size_t idx = CHANNEL * (coord[1] * Size()[0] + coord[0]);
+            return Data()[idx];
+        }
+
+        [[nodiscard]]
+        DataType* Data() noexcept
+        {
+            return static_cast<DataType*>(RawData());
+        }
+    };
+}
+
+#endif
